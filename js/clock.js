aa459345b940d189005dffa01becbeb60a300614
Nichole Dwight <nicholedwight@gmail.com>
2014-12-02 07:00:38 +0000

    fixed the errors with my syntax, seems to be working well

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (){
   (function (clockJS, $, undefined) {
 
     var currentTime, hour, minute, interval;
@@ -9,6 +9,16 @@ $(document).ready(function () {
       minute = currentTime.getMinutes();
     }
 
+    function setTime() {
+      updateTime();
+      setTimeout(function () {
+        updateTime();
+        setInterval(function() {
+          updateTime();
+        }, 60000);
+      }, interval * 1000);
+    }
+
     function updateTime() {
       getTimeValues();
 
@@ -16,39 +26,90 @@ $(document).ready(function () {
         $('.five').removeClass('active');
         $('.to').removeClass('active');
         $('.oclock').addClass('active');
-        $('.' (hour +1).toString()).addClass('active');
+        $('.' + (hour + 1).toString()).addClass('active');
       }
 
       else if (minute >= 52) {
         $('.ten').removeClass('active');
         $('.five').addClass('active');
         $('.to').addClass('active');
-        $('.' (hour +1).toString()).addClass('active');
+        $('.' + (hour + 1).toString()).addClass('active');
       }
 
       else if (minute >= 49) {
         $('.ten').addClass('active');
         $('.to').addClass('active');
         $('.quarter').removeClass('active');
-        $('.' (hour +1).toString()).addClass('active');
+        $('.' + (hour + 1).toString()).addClass('active');
       }
 
       else if (minute >= 43) {
         $('.quarter').addClass('active');
         $('.twenty').removeClass('active');
         $('.to').addClass('active');
-        $('.' (hour +1).toString()).addClass('active');
+        $('.' + (hour + 1).toString()).addClass('active');
       }
 
       else if (minute >= 36) {
         $('.twenty').addClass('active');
         $('.to').addClass('active');
-        $('.' (hour +1).toString()).addClass('active');
         $('.half').removeClass('active');
-        $('.past').removeClass('active';
+        $('.past').removeClass('active');
         $('.' + hour.toString()).removeClass('active');
         $('.' + (hour + 1).toString()).addClass('active');
       }
+
+      else if (minute >= 26) {
+        $('.half').addClass('active');
+        $('.past').addClass('active');
+        $('.twenty').removeClass('active');
+        $('.' + hour.toString()).addClass('active');
+      }
+
+      else if (minute >= 19) {
+        $('.quarter').removeClass('active');
+        $('.twenty').addClass('active');
+        $('.past').addClass('active');
+        $('.' + hour.toString()).addClass('active');
+      }
+
+      else if (minute >= 13) {
+        $('.quarter').addClass('active');
+        $('.past').addClass('active');
+        $('.ten').removeClass('active');
+        $('.' + hour.toString()).addClass('active');
+      }
+
+      else if (minute >= 9) {
+        $('.five').removeClass('active');
+        $('.ten').addClass('active');
+        $('.past').addClass('active');
+        $('.' + hour.toString()).addClass('active');
+      }
+
+      else if (minute >= 3) {
+        $('.five').addClass('active');
+        $('.past').addClass('active');
+        ('.oclock').addClass('.active');
+        $('.' + hour.toString()).addClass('active');
+      }
+
+      else {
+        $('.oclock').addClass('active');
+        $('.' + hour.toString()).addClass('active');
+      }
     }
 
-};
+    clockJS.init = function () {
+      getTimeValues();
+      interval = 60 - currentTime.getSeconds();
+      if (interval === 60) {
+        interval = 0;
+      }
+      setTime();
+    };
+
+  }(window.clockJS = window.clockJS || {}, jQuery));
+
+  clockJS.init();
+});
